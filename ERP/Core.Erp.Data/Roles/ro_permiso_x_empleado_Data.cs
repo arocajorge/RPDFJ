e338396aa3eeb82e943001dcbb66aa6bdd2ac551
carloscede <carloscede@hotmail.es>
2020-07-27 21:07:22 -0500

    ..

--- ERP/Core.Erp.Data/Roles/ro_permiso_x_empleado_Data.cs
+++ ERP/Core.Erp.Data/Roles/ro_permiso_x_empleado_Data.cs
@@ -414,9 +414,9 @@ namespace Core.Erp.Data.Roles
                     #endregion
 
                     db.SaveChanges();
-                    string sql = "update Fj_servindustrias.ro_marcaciones_x_empleado_x_incidentes_falt_Perm set id_catalogo_Cat='ASIST' where IdEmpresa='" + info.IdEmpresa + "' and IdEmpleado='" + info.IdEmpleado + "' and es_fecha_registro between '" + Convert.ToDateTime(FechaSalida).ToString("yyyy-MM-dd") + "' and '" + Convert.ToDateTime(FechaEntrada).ToString("yyyy-MM-dd") + "'";
+                    string sql = "update Fj_servindustrias.ro_marcaciones_x_empleado_x_incidentes_falt_Perm set id_catalogo_Cat='ASIST' where IdEmpresa='" + info.IdEmpresa + "' and IdEmpleado='" + info.IdEmpleado + "' and es_fecha_registro between '" + Convert.ToDateTime(FechaSalida).ToString("dd-MM-yyyy") + "' and '" + Convert.ToDateTime(FechaEntrada).ToString("dd-MM-yyyy") + "'";
                     db.Database.ExecuteSqlCommand(sql);
-                    sql = "update Fj_servindustrias.ro_marcaciones_x_empleado_x_incidentes_falt_Perm set id_catalogo_Cat='PER' where IdEmpresa='" + info.IdEmpresa + "' and IdEmpleado='" + info.IdEmpleado + "' and es_fecha_registro between '" + Convert.ToDateTime(info.FechaSalida).ToString("yyyy-MM-dd") + "' and '" + Convert.ToDateTime(info.FechaEntrada).ToString("yyyy-MM-dd") + "'";
+                    sql = "update Fj_servindustrias.ro_marcaciones_x_empleado_x_incidentes_falt_Perm set id_catalogo_Cat='PER' where IdEmpresa='" + info.IdEmpresa + "' and IdEmpleado='" + info.IdEmpleado + "' and es_fecha_registro between '" + Convert.ToDateTime(info.FechaSalida).ToString("dd-MM-yyyy") + "' and '" + Convert.ToDateTime(info.FechaEntrada).ToString("dd-MM-yyyy") + "'";
                     db.Database.ExecuteSqlCommand(sql);
 
                     
