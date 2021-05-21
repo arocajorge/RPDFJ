76157e0f2f00d4db022df81b5480e043b52044af
arocajorge <arocajorge92@gmail.com>
2021-05-20 23:39:37 -0500

    2

--- ERP_database/ERP_database/Logistica/Tables/lo_ArchivoPagoDet.sql
+++ ERP_database/ERP_database/Logistica/Tables/lo_ArchivoPagoDet.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [Logistica].[lo_ArchivoPagoDet] (
-    [IdArchivoDet] INT             NOT NULL,
+    [IdArchivoDet] INT             IDENTITY (1, 1) NOT NULL,
     [IdArchivo]    INT             NOT NULL,
     [IdNovedad]    INT             NOT NULL,
     [IdChofer]     INT             NOT NULL,
@@ -9,6 +9,8 @@
     CONSTRAINT [FK_lo_ArchivoPagoDet_lo_chofer] FOREIGN KEY ([IdChofer]) REFERENCES [Logistica].[lo_chofer] ([IdChofer]),
     CONSTRAINT [FK_lo_ArchivoPagoDet_lo_novedad] FOREIGN KEY ([IdNovedad]) REFERENCES [Logistica].[lo_novedad] ([IdNovedad])
 );
+
+
 
 
 
--- ERP_database/ERP_database/Logistica/Tables/lo_vehiculo.sql
+++ ERP_database/ERP_database/Logistica/Tables/lo_vehiculo.sql
@@ -3,7 +3,14 @@
     [IdChofer]              INT           NULL,
     [Descripcion]           VARCHAR (500) NOT NULL,
     [Placa]                 VARCHAR (50)  NOT NULL,
+    [Chasis]                VARCHAR (500) NULL,
+    [Motor]                 VARCHAR (500) NULL,
+    [Anio]                  INT           NULL,
     [Disco]                 VARCHAR (50)  NULL,
+    [Tonelaje]              INT           NULL,
+    [Ejes]                  INT           NULL,
+    [Cilindraje]            INT           NULL,
+    [IdCatalogoColor]       INT           NULL,
     [Estado]                BIT           NOT NULL,
     [EsVehiculoPropio]      BIT           NOT NULL,
     [CapacidadBines]        INT           NOT NULL,
@@ -15,8 +22,11 @@
     [FechaAnulacion]        DATETIME      NULL,
     [MotivoAnulacion]       VARCHAR (MAX) NULL,
     CONSTRAINT [PK_lo_vehiculo] PRIMARY KEY CLUSTERED ([IdVehiculo] ASC),
+    CONSTRAINT [FK_lo_vehiculo_lo_catalogo] FOREIGN KEY ([IdCatalogoColor]) REFERENCES [Logistica].[lo_catalogo] ([IdCatalogo]),
     CONSTRAINT [FK_lo_vehiculo_lo_chofer] FOREIGN KEY ([IdChofer]) REFERENCES [Logistica].[lo_chofer] ([IdChofer])
 );
+
+
 
 
 
