5baf90fa422cf175ddabbe7cbe7d7e3acef38ae7
WendyCherres <wendycherres@gmail.com>
2021-06-29 17:31:25 -0500

    k

--- ERP_database/ERP_database/Logistica/Tables/lo_GuiaEmpacadora.sql
+++ ERP_database/ERP_database/Logistica/Tables/lo_GuiaEmpacadora.sql
@@ -3,13 +3,16 @@
     [IdPlanificacion]       INT             NOT NULL,
     [IdVehiculo]            INT             NOT NULL,
     [IdChofer]              INT             NOT NULL,
-    [LibrasProgramadas]     NUMERIC (18, 2) NOT NULL,
     [IdCatalogoOrigen]      INT             NOT NULL,
     [IdCatalogoDestino]     INT             NOT NULL,
+    [IdCatalogoTipoGuia]    INT             NOT NULL,
     [Establecimiento]       VARCHAR (3)     NOT NULL,
     [PuntoEmision]          VARCHAR (3)     NOT NULL,
     [NumDocumento]          VARCHAR (20)    NOT NULL,
     [Fecha]                 DATE            NOT NULL,
+    [CapacidadLibrasTotal]  NUMERIC (18, 2) NOT NULL,
+    [CapacidadLibrasProg]   NUMERIC (18, 2) NOT NULL,
+    [CapacidadBines]        INT             NOT NULL,
     [ValorFlete]            NUMERIC (18, 2) NOT NULL,
     [ValorPorLibra]         NUMERIC (18, 2) NOT NULL,
     [ValorAFacturar]        NUMERIC (18, 2) NOT NULL,
@@ -25,10 +28,13 @@
     CONSTRAINT [PK_lo_GuiaEmpacadora] PRIMARY KEY CLUSTERED ([IdGuiaEmpacadora] ASC),
     CONSTRAINT [FK_lo_GuiaEmpacadora_lo_catalogo] FOREIGN KEY ([IdCatalogoOrigen]) REFERENCES [Logistica].[lo_catalogo] ([IdCatalogo]),
     CONSTRAINT [FK_lo_GuiaEmpacadora_lo_catalogo1] FOREIGN KEY ([IdCatalogoDestino]) REFERENCES [Logistica].[lo_catalogo] ([IdCatalogo]),
+    CONSTRAINT [FK_lo_GuiaEmpacadora_lo_catalogo2] FOREIGN KEY ([IdCatalogoTipoGuia]) REFERENCES [Logistica].[lo_catalogo] ([IdCatalogo]),
     CONSTRAINT [FK_lo_GuiaEmpacadora_lo_chofer] FOREIGN KEY ([IdChofer]) REFERENCES [Logistica].[lo_chofer] ([IdChofer]),
     CONSTRAINT [FK_lo_GuiaEmpacadora_lo_PlanificacionPesca] FOREIGN KEY ([IdPlanificacion]) REFERENCES [Logistica].[lo_PlanificacionPesca] ([IdPlanificacion]),
     CONSTRAINT [FK_lo_GuiaEmpacadora_lo_vehiculo] FOREIGN KEY ([IdVehiculo]) REFERENCES [Logistica].[lo_vehiculo] ([IdVehiculo])
 );
+
+
 
 
 
