4313370220ad04acbc25c34aef67311078e21ed7
arocajorge <arocajorge92@gmail.com>
2021-07-02 10:35:27 -0500

    1

--- ERP_database/ERP_database/Logistica/Tables/lo_factura_lo_Guia.sql
+++ ERP_database/ERP_database/Logistica/Tables/lo_factura_lo_Guia.sql
@@ -1,14 +1,20 @@
 ﻿CREATE TABLE [Logistica].[lo_factura_lo_Guia] (
-    [IdRelacion]       INT          IDENTITY (1, 1) NOT NULL,
-    [IdEmpresa]        INT          NOT NULL,
-    [IdSucursal]       INT          NOT NULL,
-    [IdBodega]         INT          NOT NULL,
-    [IdCbteVta]        NUMERIC (18) NOT NULL,
-    [IdGuiaEmpacadora] INT          NOT NULL,
+    [IdRelacion]       INT             IDENTITY (1, 1) NOT NULL,
+    [IdEmpresa]        INT             NOT NULL,
+    [IdSucursal]       INT             NOT NULL,
+    [IdBodega]         INT             NOT NULL,
+    [IdCbteVta]        NUMERIC (18)    NOT NULL,
+    [IdGuiaEmpacadora] INT             NOT NULL,
+    [CantidadLibras]   NUMERIC (18, 2) NOT NULL,
+    [ValorLibra]       NUMERIC (18, 2) NOT NULL,
+    [ValorFlete]       NUMERIC (18, 2) NOT NULL,
+    [ValorFacturar]    NUMERIC (18, 2) NOT NULL,
     CONSTRAINT [PK_lo_factura_lo_Guia_1] PRIMARY KEY CLUSTERED ([IdRelacion] ASC),
     CONSTRAINT [FK_lo_factura_lo_Guia_fa_factura] FOREIGN KEY ([IdEmpresa], [IdSucursal], [IdBodega], [IdCbteVta]) REFERENCES [dbo].[fa_factura] ([IdEmpresa], [IdSucursal], [IdBodega], [IdCbteVta]),
     CONSTRAINT [FK_lo_factura_lo_Guia_lo_GuiaEmpacadora] FOREIGN KEY ([IdGuiaEmpacadora]) REFERENCES [Logistica].[lo_GuiaEmpacadora] ([IdGuiaEmpacadora])
 );
+
+
 
 
 
