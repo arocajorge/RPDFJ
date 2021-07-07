fb2a8f1b51a0a646708dc69feda0dd9a46bfef5d
arocajorge <arocajorge92@gmail.com>
2021-07-06 21:30:58 -0500

    w

---
+++ ERP_database/ERP_database/Logistica/Tables/lo_planificacionPescaHistorico.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Logistica].[lo_planificacionPescaHistorico] (
+    [IdHistorico]     INT           IDENTITY (1, 1) NOT NULL,
+    [IdPlanificacion] INT           NOT NULL,
+    [Fecha]           DATETIME      NOT NULL,
+    [IdUsuario]       VARCHAR (200) NOT NULL,
+    [Observacion]     VARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_lo_planificacionPescaHistorico] PRIMARY KEY CLUSTERED ([IdHistorico] ASC)
+);
+
