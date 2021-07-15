94641b276442380d7fcacd9b8b0df02018e22c02
arocajorge <arocajorge92@gmail.com>
2021-07-14 23:10:24 -0500

    3

---
+++ ERP_database/ERP_database/Logistica/Tables/lo_GuiaDetCodigo.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Logistica].[lo_GuiaDetCodigo] (
+    [IdCodigo]  INT           IDENTITY (1, 1) NOT NULL,
+    [IdGuiaDet] INT           NOT NULL,
+    [Codigo]    VARCHAR (MAX) NOT NULL,
+    [Fecha]     DATETIME      NOT NULL,
+    [IdUsuario] VARCHAR (500) NOT NULL,
+    CONSTRAINT [PK_lo_GuiaDetCodigo] PRIMARY KEY CLUSTERED ([IdCodigo] ASC),
+    CONSTRAINT [FK_lo_GuiaDetCodigo_lo_GuiaDet] FOREIGN KEY ([IdGuiaDet]) REFERENCES [Logistica].[lo_GuiaDet] ([IdGuiaDet])
+);
+
